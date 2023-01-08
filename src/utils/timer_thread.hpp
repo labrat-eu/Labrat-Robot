2a8e0dafed9f6493b08c752f7df540192d5e6ef5
Max Zimmermann <maxyvon@gmx.de>
2023-01-08 08:12:29 +0100

    [src] Refractor timer thread implementation to use jthreads
    Refractor timer thread implementation to use jthreads.

--- src/utils/timer_thread.hpp
+++ src/utils/timer_thread.hpp
@@ -1,73 +1,43 @@
 #pragma once
 
-#include <atomic>
 #include <chrono>
 #include <functional>
-#include <memory>
 #include <thread>
 
 inline namespace utils {
 
 class TimerThread {
 public:
-  TimerThread() {
-    skip_cleanup = true;
-  }
-
+  TimerThread() = default;
   TimerThread(TimerThread &) = delete;
 
-  TimerThread(TimerThread &&rhs) : thread(std::move(rhs.thread)), exit_flag(rhs.exit_flag) {
-    skip_cleanup = rhs.skip_cleanup;
-    rhs.skip_cleanup = true;
-  };
+  TimerThread(TimerThread &&rhs) : thread(std::move(rhs.thread)) {};
 
   template <typename Function, typename R, typename P, typename... Args>
   TimerThread(Function &&function, const std::chrono::duration<R, P> &interval, Args &&...args) {
-    exit_flag = std::make_shared<std::atomic_flag>();
-
-    thread = std::thread(
-      [interval](Function &&function, std::shared_ptr<std::atomic_flag> &&exit_flag, Args &&...args) {
+    thread = std::jthread(
+      [interval](std::stop_token token, Function &&function, Args &&...args) {
       while (true) {
         const std::chrono::time_point<std::chrono::steady_clock> time_begin = std::chrono::steady_clock::now();
 
         std::invoke<Function, Args...>(std::forward<Function>(function), std::forward<Args>(args)...);
 
-        if (exit_flag->test()) {
+        if (token.stop_requested()) {
           break;
         }
 
         std::this_thread::sleep_until(time_begin + interval);
       }
       },
-      std::forward<Function>(function), std::shared_ptr<std::atomic_flag>(exit_flag), std::forward<Args>(args)...);
-
-    skip_cleanup = false;
-  }
-
-  ~TimerThread() {
-    if (!skip_cleanup) {
-      exit_flag->test_and_set();
-      thread.join();
-    }
+      std::forward<Function>(function), std::forward<Args>(args)...);
   }
 
   void operator=(TimerThread &&rhs) {
-    if (!skip_cleanup) {
-      exit_flag->test_and_set();
-      thread.join();
-    }
-
     thread = std::move(rhs.thread);
-    exit_flag = rhs.exit_flag;
-
-    skip_cleanup = rhs.skip_cleanup;
-    rhs.skip_cleanup = true;
   };
 
 private:
-  std::thread thread;
-  std::shared_ptr<std::atomic_flag> exit_flag;
-  bool skip_cleanup;
+  std::jthread thread;
 };
 
 }  // namespace utils
