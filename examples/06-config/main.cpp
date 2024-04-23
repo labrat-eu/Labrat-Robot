6a43bcf47e024300c83ffa1b0464a04c628c92de
Max Yvon Zimmermann <max.yvon.zimmermann@campus.tu-berlin.de>
2024-04-23 21:18:30 +0200

    [src] Remove redundant includes

--- examples/06-config/main.cpp
+++ examples/06-config/main.cpp
@@ -1,13 +1,10 @@
 #include <labrat/lbot/config.hpp>
-#include <labrat/lbot/manager.hpp>
-#include <labrat/lbot/plugins/foxglove-ws/server.hpp>
-#include <labrat/lbot/utils/signal.hpp>
+#include <labrat/lbot/logger.hpp>
 
 #include <cstdint>
 
 int main(int argc, char **argv) {
   lbot::Logger logger("main");
-  lbot::Manager::Ptr manager = lbot::Manager::get();
 
   lbot::Config::Ptr config = lbot::Config::get();
   config->load("06-config/config.yaml");
