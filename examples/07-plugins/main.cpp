18e18a5a23812af1b44e35c20b1a03482ccb12dc
Max Yvon Zimmermann <max.yvon.zimmermann@campus.tu-berlin.de>
2024-08-22 20:32:17 +0200

    [src] Add process data to linux stats

--- examples/07-plugins/main.cpp
+++ examples/07-plugins/main.cpp
@@ -6,69 +6,12 @@
 #include <labrat/lbot/utils/signal.hpp>
 #include <labrat/lbot/utils/thread.hpp>
 
-#include <cmath>
-
-#include <examples/msg/vector.hpp>
-
 // Plugins
 //
 // Plugins allow you to globally access messages.
 // This is useful if you want to record messages for later analysis or if you want to debug you program while it is running.
 //
 // In this example we will use some existing plugins that might help you in your projects.
-
-class SenderNode : public lbot::Node {
-public:
-  SenderNode() {
-    sender = addSender<lbot::Message<examples::msg::Vector>>("/examples/number");
-
-    sender_thread = lbot::TimerThread(&SenderNode::senderFunction, std::chrono::milliseconds(50), "sender_thread", 1, this);
-  }
-
-private:
-  void senderFunction() {
-    ++i;
-
-    lbot::Message<examples::msg::Vector> message;
-    message.x = std::sin(i / 100.0);
-    message.y = std::cos(i / 100.0);
-
-    sender->put(message);
-  }
-
-  Sender<lbot::Message<examples::msg::Vector>>::Ptr sender;
-  lbot::TimerThread sender_thread;
-
-  uint64_t i = 0;
-};
-
-class ServerNode : public lbot::Node {
-public:
-  ServerNode() {
-    // Register a server on the service with the name "/examples/power" and the handler ServerNode::handleRequest().
-    // There can only be one server per service.
-    // The type of this server must match any previously registered client on the same service.
-    server = addServer<examples::msg::Vector, examples::msg::Vector>("/examples/mirror");
-    server->setHandler(&ServerNode::handleRequest);
-  }
-
-private:
-  // When a request has been made, this function will be called to respond.
-  // Handler functions must be static.
-  static lbot::Message<examples::msg::Vector> handleRequest(const lbot::Message<examples::msg::Vector> &request) {
-    // Construct a response message.
-    lbot::Message<examples::msg::Vector> response;
-    response.x = request.y;
-    response.y = request.x;
-
-    return response;
-  }
-
-  // A service consists of two messages.
-  // One is the request sent from the client to the server.
-  // The other is the response sent from the server to the client.
-  Server<examples::msg::Vector, examples::msg::Vector>::Ptr server;
-};
 
 int main(int argc, char **argv) {
   lbot::Logger logger("main");
@@ -91,9 +34,6 @@ int main(int argc, char **argv) {
   //  1. Install and open Foxglove Studio (https://foxglove.dev/)
   //  2. Open a connection via Foxglove WebSocket with the URL ws://localhost:8765
   manager->addPlugin<lbot::plugins::FoxgloveServer>("foxglove-ws");
-
-  manager->addNode<SenderNode>("sender");
-  manager->addNode<ServerNode>("server");
 
   logger.logInfo() << "Press CTRL+C to exit the program.";
 
--- src/plugins/linux/stats.cpp
+++ src/plugins/linux/stats.cpp
@@ -12,6 +12,7 @@
 #include <labrat/lbot/plugins/linux/msg/cpu.hpp>
 #include <labrat/lbot/plugins/linux/msg/disks.hpp>
 #include <labrat/lbot/plugins/linux/msg/memory.hpp>
+#include <labrat/lbot/plugins/linux/msg/process.hpp>
 #include <labrat/lbot/plugins/linux/stats.hpp>
 #include <labrat/lbot/utils/thread.hpp>
 
@@ -22,7 +23,10 @@
 #include <unordered_map>
 #include <forward_list>
 #include <charconv>
+#include <filesystem>
+#include <cmath> 
 
+#include <linux/version.h>
 #include <sys/statvfs.h>
 
 inline namespace labrat {
@@ -56,6 +60,77 @@ struct CpuRawCounters {
   }
 };
 
+struct ProcessRawCounters {
+  uint64_t user_time;
+  uint64_t system_time;
+
+  ProcessRawCounters operator-(const ProcessRawCounters &rhs) const {
+    return ProcessRawCounters{
+      user_time - rhs.user_time,
+      system_time - rhs.system_time,
+    };
+  }
+};
+
+inline ProcessState charToProcessState(char c) {
+  switch (c) {
+    case 'R': {
+      return ProcessState::Running;
+    }
+    case 'S': {
+      return ProcessState::Sleeping;
+    }
+    case 'D': {
+      return ProcessState::Waiting;
+    }
+    case 'Z': {
+      return ProcessState::Zombie;
+    }
+    case 'T': {
+      return ProcessState::Stopped;
+    }
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,33)
+    case 't': {
+      return ProcessState::Tracing;
+    }
+#endif
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,0)
+    case 'W': {
+      return ProcessState::Paging;
+    }
+#endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,0)
+    case 'X': {
+      return ProcessState::Dead;
+    }
+#endif
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,13,0) && LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,33)
+    case 'x': {
+      return ProcessState::Dead;
+    }
+    case 'K': {
+      return ProcessState::Wakekill;
+    }
+    case 'W': {
+      return ProcessState::Waking;
+    }
+#endif
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,13,0) && LINUX_VERSION_CODE >= KERNEL_VERSION(3,9,0)
+    case 'P': {
+      return ProcessState::Parked;
+    }
+#endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,14,0)
+    case 'I': {
+      return ProcessState::Idle;
+    }
+#endif
+    default: {
+      return ProcessState::Unknown;
+    }
+  }
+}
+
 class LinuxStatsNode : public UniqueNode {
 public:
   LinuxStatsNode();
@@ -66,14 +141,19 @@ private:
   void loopSlow();
 
   void readCpu();
+  void readProcess();
   void readMemory();
   void readDisks();
   
   Sender<Cpu>::Ptr sender_cpu;
+  Sender<Process>::Ptr sender_process;
   Sender<Memory>::Ptr sender_memory;
   Sender<Disks>::Ptr sender_disks;
 
-  std::unordered_map<std::string, CpuRawCounters> cpu_counter_map;
+  std::unordered_map<int, CpuRawCounters> cpu_counter_map;
+  float total_cpu_time;
+
+  std::unordered_map<int, ProcessRawCounters> process_counter_map;
 
   TimerThread thread_fast;
   TimerThread thread_slow;
@@ -87,8 +167,11 @@ LinuxStats::~LinuxStats() = default;
 
 LinuxStatsNode::LinuxStatsNode() {
   sender_cpu = addSender<Cpu>("/linux/cpu");
+  sender_process = addSender<Process>("/linux/process");
   sender_memory = addSender<Memory>("/linux/memory");
   sender_disks = addSender<Disks>("/linux/disks");
+
+  total_cpu_time = std::numeric_limits<float>::quiet_NaN();
 
   thread_fast = TimerThread(&LinuxStatsNode::loopFast, std::chrono::seconds(2), "linux fast", 1, this);
   thread_slow = TimerThread(&LinuxStatsNode::loopSlow, std::chrono::seconds(30), "linux slow", 1, this);
@@ -98,6 +181,7 @@ LinuxStatsNode::~LinuxStatsNode() = default;
 
 void LinuxStatsNode::loopFast() {
   readCpu();
+  readProcess();
   readMemory();
 }
 
@@ -108,28 +192,38 @@ void LinuxStatsNode::loopSlow() {
 void LinuxStatsNode::readCpu() {
   Message<Cpu> message;
 
+  if (!cpu_counter_map.empty()) {
+    message.cores.reserve(cpu_counter_map.size() - 1);
+  }
+
   std::ifstream info("/proc/stat");
 
   if (!info) {
     throw SystemException("Failed to open /proc/stat");
   }
 
-  std::string line;
-  while (std::getline(info, line)) {
-    if (!line.starts_with("cpu")) {
+  std::array<char, 1024> buffer;
+  while (info.getline(buffer.data(), buffer.size())) {
+    if (std::strncmp("cpu", buffer.data(), sizeof("cpu") - 1)) {
       continue;
     }
 
-    std::istringstream linestream(line);
-
-    std::string name;
+    int id = -1;
     CpuRawCounters counters;
     
-    linestream >> name >> counters.user_time >> counters.nice_time >> counters.system_time >> counters.idle_time >> counters.io_wait >> counters.irq >> counters.soft_irq >> counters.steal >> counters.guest >> counters.guest_nice;
+    if (buffer.data()[3] == ' ') {
+      if (!std::sscanf(buffer.data() + 4, "%lu %lu %lu %lu %lu %lu %lu %lu %lu %lu", &counters.user_time, &counters.nice_time, &counters.system_time, &counters.idle_time, &counters.io_wait, &counters.irq, &counters.soft_irq, &counters.steal, &counters.guest, &counters.guest_nice)) {
+        throw SystemException("Failed to parse CPU stats", errno);
+      }
+    } else {
+      if (!std::sscanf(buffer.data() + 3, "%d %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu", &id, &counters.user_time, &counters.nice_time, &counters.system_time, &counters.idle_time, &counters.io_wait, &counters.irq, &counters.soft_irq, &counters.steal, &counters.guest, &counters.guest_nice)) {
+        throw SystemException("Failed to parse core stats", errno);
+      }
+    }
 
-    std::unordered_map<std::string, CpuRawCounters>::iterator iter = cpu_counter_map.find(name);
+    std::unordered_map<int, CpuRawCounters>::iterator iter = cpu_counter_map.find(id);
     if (iter == cpu_counter_map.end()) {
-      cpu_counter_map.emplace(name, counters);
+      cpu_counter_map.emplace(id, counters);
       continue;
     }
 
@@ -141,20 +235,15 @@ void LinuxStatsNode::readCpu() {
     const float kernel_load = (float)diff.system_time / total_time;
     const float load = user_load + kernel_load;
 
-    std::string_view id_view(name.begin() + 3, name.end());
-
-    if (id_view.empty()) {
+    if (id == -1) {
       message.load = load;
       message.user_load = user_load;
       message.kernel_load = kernel_load;
+
+      total_cpu_time = total_time;
     } else {
       std::unique_ptr<CoreNative> core = std::make_unique<CoreNative>();
-
-      std::from_chars_result result = std::from_chars(id_view.data(), id_view.data() + id_view.length(), core->id);
-      if (result.ec != std::errc()) {
-        throw SystemException("Failed to parse CPU id");
-      }
-
+      core->id = id;
       core->load = load;
       core->user_load = user_load;
       core->kernel_load = kernel_load;
@@ -164,6 +253,89 @@ void LinuxStatsNode::readCpu() {
   }
 
   sender_cpu->put(std::move(message));
+}
+
+void LinuxStatsNode::readProcess() {
+  const int cpu_count = cpu_counter_map.size() - 1;
+  if (cpu_count < 1 || std::isnan(total_cpu_time)) {
+    return;
+  }
+
+  const float total_time_per_cpu = total_cpu_time / cpu_count; 
+
+  Message<Process> message;
+
+  if (!process_counter_map.empty()) {
+    message.threads.reserve(process_counter_map.size());
+  }
+
+  for (const std::filesystem::directory_entry &task : std::filesystem::directory_iterator("/proc/self/task/")) {
+    std::ifstream info(task.path() / std::filesystem::path("stat"));
+
+    if (!info) {
+      continue;
+    }
+
+    std::array<char, 1024> buffer;
+    if (!info.getline(buffer.data(), buffer.size())) {
+      continue;
+    }
+
+    std::unique_ptr<ThreadNative> thread = std::make_unique<ThreadNative>();
+    std::array<char, 32> name;
+    char state;
+    int parent_id;
+    int group_id;
+    int session_id;
+    int tty;
+    int foreground_group_id;
+    unsigned int flags;
+    long minor_faults;
+    long children_minor_faults;
+    long major_faults;
+    long children_major_faults;
+    ProcessRawCounters counters;
+    ProcessRawCounters children_counters;
+    long priority;
+    long nice;
+
+    if (!std::sscanf(buffer.data(), "%d", &thread->id)) {
+      throw SystemException("Failed to parse thread id", errno);
+    }
+
+    char *const name_start = std::strchr(buffer.data(), '(') + 1;
+    char *const name_end = std::strrchr(buffer.data(), ')');
+    if (name_start == nullptr || name_end == nullptr) {
+      throw SystemException("Failed to parse thread name");
+    }
+
+    *name_end = '\0';
+    strncpy(name.data(), name_start, name.size() - 1);
+    name.back() = '\0';
+
+    if (!std::sscanf(name_end + 2, "%c %d %d %d %d %d %u %lu %lu %lu %lu %lu %lu %ld %ld %ld %ld", &state, &parent_id, &group_id, &session_id, &tty, &foreground_group_id, &flags, &minor_faults, &children_minor_faults, &major_faults, &children_major_faults, &counters.user_time, &counters.system_time, &children_counters.user_time, &children_counters.system_time, &priority, &nice)) {
+      throw SystemException("Failed to parse thread stats", errno);
+    }
+
+    std::unordered_map<int, ProcessRawCounters>::iterator iter = process_counter_map.find(thread->id);
+    if (iter == process_counter_map.end()) {
+      process_counter_map.emplace(thread->id, counters);
+      continue;
+    }
+
+    const ProcessRawCounters diff = counters - iter->second;
+    iter->second = counters;
+
+    thread->name = std::string(name.data());
+    thread->state = charToProcessState(state);
+    thread->user_load = (float)diff.user_time / total_time_per_cpu;
+    thread->kernel_load = (float)diff.system_time / total_time_per_cpu;
+    thread->load = thread->user_load + thread->kernel_load;
+
+    message.threads.emplace_back(std::move(thread));
+  }
+
+  sender_process->put(std::move(message));
 }
 
 void LinuxStatsNode::readMemory() {
@@ -235,10 +407,8 @@ void LinuxStatsNode::readDisks() {
 
     std::string mount_point;
     std::string attributes;
-    std::string dummy1;
-    std::string dummy2;
-
-    linestream >> disk->name >> mount_point >> disk->file_system >> attributes >> dummy1 >> dummy2;
+    
+    linestream >> disk->name >> mount_point >> disk->file_system >> attributes;
 
     static const std::unordered_set<std::string> supported_file_systems = {
       "ext2", "ext3", "ext4", "vfat", "ntfs", "zfs", "hfs", "reiserfs", "reiser4", "fuseblk", "exfat", "f2fs", "hfs+", "jfs", "btrfs", "bcachefs", "minix", "nilfs", "xfs", "apfs"
